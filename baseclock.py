3de59094a7b2030e5aa8b441d19cf19bf08d05fa
Paul <paulsajo0@gmail.com>
2021-07-11 16:12:49 +0530

    small fixes in baseclock.py

--- baseclock.py
+++ baseclock.py
@@ -2,21 +2,32 @@ import tkinter
 import math
 
 
-class Clock:
+class BaseClock:
 
     def __init__(self, canvas: tkinter.Canvas, min: int = None, max: int = None,
                  step=0, replace_step: bool = True, numberlst: list = [], **kwargs):
 
         self.options = {
+            "min-size": 200,
+            "max-size": 1080,
             "bg": "#ffffff",
             "bdColor": "#000000",
             "bdwidth": 1,
             "textoffset": 20,
             "textfont": ("Times", 20, "normal"),
+            "textcolor": "#878787",
+            "alttextwidth": 10,
             "start": 0,
-            "defaultPointer": 0, # which number should it be pointing to in the beginning
-            "headsize": 20
+            "defaultPointer": 0,  # which number should it be pointing to in the beginning
+            "handthickness": 3,
+            "handcolor": "#000000",
+            "capstyle": "round",
+            "headsize": 20,
+            "headcolor": "#000000"
         }
+
+        if set(kwargs)-set(self.options):
+            raise TypeError(f"Got unexpected arguments '{', '.join(set(kwargs)-set(self.options))}'")
 
         self.options.update(kwargs)
 
@@ -25,8 +36,12 @@ class Clock:
                                              outline=self.options["bdColor"],
                                              width=self.options["bdwidth"])
 
-        self.hand_line = self.canvas.create_line(0, 0, 0, 0, fill='black', width=2)
-        self.hand_end = self.canvas.create_oval(0, 0, 0, 0, fill='red')
+        self.hand_line = self.canvas.create_line(0, 0, 0, 0,
+                                                 fill=self.options["handcolor"],
+                                                 width=self.options["handthickness"],
+                                                 capstyle=self.options["capstyle"])
+
+        self.hand_end = self.canvas.create_oval(0, 0, 0, 0, fill=self.options["headcolor"])
 
         if min and max:
             self.numberlst = range(min, max)
@@ -55,10 +70,11 @@ class Clock:
         for index, char in enumerate(self.numberlst, start=self.options["start"]):
 
             if index % self.step != 0:
-                obj = self.canvas.create_oval(0, 0, 5, 5, width=10, tags="text")
+                obj = self.canvas.create_oval(0, 0, 5, 5, width=1, tags="text", fill=self.options["textcolor"])
 
             else:
-                obj = self.canvas.create_text(0, 0, text=f"{char}", tags="text", font=self.options["textfont"])
+                obj = self.canvas.create_text(0, 0, text=f"{char}", tags="text",
+                                              font=self.options["textfont"], fill=self.options["textcolor"])
 
             if self.current_index == char:
                 self.current_id = obj
@@ -68,7 +84,8 @@ class Clock:
         width, height = event.width, event.height
         size = width if width < height else height
 
-        if size > 200:  # doesn't shrink if the size is smaller than 200
+        if self.options["min-size"] < size < self.options[
+            "max-size"]:  # doesn't shrink or expand is the size is not b/w min and max size
             centerX, centerY = width / 2, height / 2
             size -= float(self.canvas.itemcget(self.clock, "width")) + 10
 
@@ -86,7 +103,10 @@ class Clock:
                 x = centerX + radius * math.cos(_angle)
 
                 if index % self.step != 0:
-                    self.canvas.coords(obj, x, y, x + 5, y + 5)
+                    self.canvas.coords(obj, x - self.options["alttextwidth"],
+                                       y - self.options["alttextwidth"],
+                                       x + self.options["alttextwidth"],
+                                       y + self.options["alttextwidth"])
                     continue
 
                 self.canvas.coords(obj, x, y)
@@ -94,6 +114,10 @@ class Clock:
             self.updateHand()
 
     def configure(self, **kwargs):  # background, border-color, border-width
+
+        if set(kwargs)-set(self.options):
+            raise TypeError(f"Got unexpected arguments '{', '.join(set(kwargs)-set(self.options))}'")
+
         self.options.update(kwargs)
 
         self.canvas.itemconfig(self.clock,
@@ -101,10 +125,27 @@ class Clock:
                                outline=self.options['bdColor'],
                                width=self.options['bdwidth'])
 
+        for obj in self.canvas.find_withtag("text"):
+            try:
+                self.canvas.itemconfig(obj, font=self.options["textfont"], fill=self.options["textcolor"])
+
+            except tkinter.TclError:
+                self.canvas.itemconfig(obj, fill=self.options["textcolor"])
+
+        self.canvas.itemconfig(self.hand_line, fill=self.options["handcolor"],
+                               width=self.options["handthickness"],
+                               capstyle=self.options["capstyle"])
+
+        self.canvas.itemconfig(self.hand_line, fill=self.options["headcolor"])
+
+    def current(self):
+        return self.current_index
+
     def movehand(self, event: tkinter.Event):
 
         self.current_id = self.canvas.find_withtag('current')[0]
         self.updateHand()
+        self.canvas.event_generate("<<Changed>>")
 
     def updateHand(self):
 
@@ -130,8 +171,8 @@ class Clock:
                 if self.current_id == obj:
                     self.current_index = str(number)
 
-
-        print(self.current_index)
+    def bind(self, seq, callback):
+        self.canvas.bind(seq, callback)
 
 
 if __name__ == "__main__":
@@ -141,5 +182,7 @@ if __name__ == "__main__":
     canvas1['bg'] = 'white'
     canvas1.pack(fill='both', expand=1)
 
-    clock = Clock(canvas1, min=1, max=13, step=2, bdwidth=10, textoffset=40, start=-2)
+    clock = BaseClock(canvas1, min=1, max=13, step=2, bdwidth=10, textoffset=40, start=-2)
+    clock.configure(handthickness=7, textcolor= "blue", textfont=("Times", 30, "bold"), alttextwidth=10)
+    clock.bind("<<Changed>>", lambda a: print(clock.current()))
     root.mainloop()
